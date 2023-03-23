5490eeeee0af0c9e21ffa29eef5e89293c26c5ce
mkcmkcmkc <31344997+mkcmkcmkc@users.noreply.github.com>
2023-03-23 12:21:32 +0100

    Create DBconnection.py

---
+++ DBconnection.py
@@ -0,0 +1,12 @@
+# Importing module
+import mysql.connector
+ 
+# Creating connection object
+mydb = mysql.connector.connect(
+    host = "12.12.12.1",
+    user = "Admin",
+    password = "nndhdee08HHHHHSB00873kaajhd##¤#"11"
+)
+ 
+# Printing the connection object
+print(mydb)
